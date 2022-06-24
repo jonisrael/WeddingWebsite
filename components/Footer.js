c45f9108c12c65a68e86464bc526956dbc9a023f
jonisrael <72095830+jonisrael@users.noreply.github.com>
2022-06-24 11:25:34 -0500

    update date

--- components/Footer.js
+++ components/Footer.js
@@ -3,6 +3,7 @@ import html from "html-literal";
 export default () => html`
   <footer id="footer">
     Website Created by Jonathan Israel. <br />
-    <strong>RSVP and Registry Pages Coming Soon!</strong>
+    <strong>RSVP and Registry Pages Coming Soon!</strong><br />
+    Last updated June 24, 2022
   </footer>
 `;
