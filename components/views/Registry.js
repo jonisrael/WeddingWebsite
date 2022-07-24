a985fe191eb860c26f7b1455c4faa565359cebe4
jonisrael <72095830+jonisrael@users.noreply.github.com>
2022-07-24 14:34:40 -0400

    added registry

--- components/views/Registry.js
+++ components/views/Registry.js
@@ -2,9 +2,19 @@ import html from "html-literal";
 
 export default (st) => html`
   <section id="registry">
-    <p style="font-size:small">
-      Our registry can be found at the Knot. Click this link to be taken to it!
-      https://registry.theknot.com/susanna-bowers-jonathan-israel-november-2022-mo/52790054
-    </p>
+    <div id="registry-text">
+      <p style="text-align: center; font-weight: bold; font-size: x-large;">
+        Our registry can be found at the Knot.com. <br />Click the button below
+        to access it!
+      </p>
+    </div>
+    <div class="registry-button-section">
+      <a
+        class="default-button"
+        href="https://registry.theknot.com/susanna-bowers-jonathan-israel-november-2022-mo/52790054"
+        >Registry</a
+      >
+    </div>
+    <img id="supporting-photo"></img>
   </section>
 `;
