f81399b38fb0a8f6f6dba1457c6a1f8b86539e6e
jonisrael <72095830+jonisrael@users.noreply.github.com>
2022-09-20 20:39:07 -0500

    asdffdsa

--- rsvpFormLinks.js
+++ rsvpFormLinks.js
@@ -15,10 +15,39 @@ export const rsvpForm = {
   },
   4: {
     hyperlink: `https://forms.gle/VfYFQACzL4UGQ9JS6`,
-    embed: `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfe9YimYoautSE2pe0Q0xMaiD44Mt9Q-7R3IOz3Sd5CHAsIlQ/viewform?embedded=true" width="80%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`,
+    embed: `<iframe src="https://docs.google.com/forms/d/e/1FAIpQL Sfe9YimYoautSE2pe0Q0xMaiD44Mt9Q-7R3IOz3Sd5CHAsIlQ/viewform?embedded=true" width="80%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`,
   },
   5: {
     hyperlink: `https://forms.gle/yc2TpEPv66QrrYi77`,
     embed: `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfTETOlSicm-LHrS06-oGaBRAVjolLIEbXtmHjH7DWheGswMg/viewform?embedded=true" width="80%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`,
   },
 };
+
+rsvpForm["2"] = JSON.parse(JSON.stringify(rsvpForm["4"]));
+rsvpForm["3"] = JSON.parse(JSON.stringify(rsvpForm["4"]));
+rsvpForm["5"] = JSON.parse(JSON.stringify(rsvpForm["4"]));
+
+// hyperlinks updated 9-6-2022
+
+// export const rsvpForm = {
+//   1: {
+//     hyperlink: `https://forms.gle/6mcFZ9guPMbqKZBe9`,
+//     embed: `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdw1qiXrwApCubvhooQOU-VHySA1t1_4EZVW2IDBUumaZOVXw/viewform?embedded=true" width="80%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`,
+//   },
+//   2: {
+//     hyperlink: `https://forms.gle/5EKSMigECpUYtV6f8`,
+//     embed: `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeSHHc7EHlQnKrbCG8mk1jisMxiUgH5eKKwo78H1pOlQjbgPQ/viewform?embedded=true" width="80%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`,
+//   },
+//   3: {
+//     hyperlink: `https://forms.gle/J7JBiG9ZkogTqjMw5`,
+//     embed: `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeinrOiO-UoC3lNkCKyBQVtD6KI4P2FyuhdRVSH0JatDja2SQ/viewform?embedded=true" width="80%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`,
+//   },
+//   4: {
+//     hyperlink: `https://forms.gle/VfYFQACzL4UGQ9JS6`,
+//     embed: `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfe9YimYoautSE2pe0Q0xMaiD44Mt9Q-7R3IOz3Sd5CHAsIlQ/viewform?embedded=true" width="80%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`,
+//   },
+//   5: {
+//     hyperlink: `https://forms.gle/yc2TpEPv66QrrYi77`,
+//     embed: `<iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfTETOlSicm-LHrS06-oGaBRAVjolLIEbXtmHjH7DWheGswMg/viewform?embedded=true" width="80%" height="100%" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>`,
+//   },
+// };
